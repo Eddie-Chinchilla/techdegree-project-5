d3e0d57d3e089208897703555e7e3adb71020eb8
Eddie-Chinchilla <84281367+Eddie-Chinchilla@users.noreply.github.com>
2021-09-17 11:05:54 -0500

    finished project

---
+++ js/search.js
@@ -0,0 +1,25 @@
+
+const search = document.getElementById("search");
+search.addEventListener("keyup", searchFilter);
+
+function searchFilter() {
+    let x = search.value.toLowerCase();
+        console.log(x);
+
+    let photos = document.getElementsByClassName("photo");
+
+    for (i = 0; i < photos.length; i++ ) { 
+        var cap = photos[i].getAttribute("data-caption").toLowerCase();
+        if (cap.includes(x)) {
+            photos[i].classList.add("show");
+            photos[i].classList.remove("hide");
+            console.log(cap);
+        } else {
+            photos[i].classList.add("hide");
+            photos[i].classList.remove("show");
+        }
+    }
+}
+
+
+
